4f0505e199992ebf3ab5a79536591bf645d81f95
yvandouenel <y.douenel@coopernet.fr>
2023-11-09 12:26:48 +0100

    Gestion de l'ordre des tâches.

--- react/tasks/src/components/App.jsx
+++ react/tasks/src/components/App.jsx
@@ -7,7 +7,7 @@ function App() {
   // Déclaration de l'état tasks qui est un tableau d'objets de task
   const [tasks, setTasks] = useState([
     { id: 1, name: "Faire la vaisselle", done: false },
-    { id: 2, name: "Essuyer la vaisselle", done: false },
+    { id: 2, name: "Essuyer la vaisselle", done: true },
     { id: 3, name: "Ranger la vaisselle", done: true }
   ]);
 
@@ -30,11 +30,32 @@ function App() {
     // Modification du state en utilisant setTasks
     setTasks(tasksUpdated);
   }
+  function handleSubmitAdd(taskName) {
+    console.log(`Dans handleSubmitAdd, id : `);
+    // Ajout d'une tâche dans la copie de tasks
+    const tasksUpdated = [...tasks, { name: taskName, id: tasks.length + 1, done: false }];
+
+    // Modification du state en utilisant setTasks
+    setTasks(tasksUpdated);
+  }
 
   return (
     <div className="App container">
       <h1>Gestions des tâches</h1>
-      {tasks.map((task) =>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          // Récupération de la value de l'input
+          const taskName = document.querySelector("#title").value;
+          console.log(`taskName :`, taskName);
+          handleSubmitAdd(taskName);
+        }}
+        className='d-flex gap-2 align-items-center'>
+        <label htmlFor="title" className='form-label'>Tâche : </label>
+        <input type="text" id="title" className='form-control w-50' />
+        <input type="submit" value="Ajouter" className='btn btn-success' />
+      </form>
+      {[...tasks].sort((a,b) => b.id - a.id ).sort((a,b) => a.done - b.done ).map((task) =>
         <Task
           key={task.id}
           task={task}
